77298bd749a8c5eae0a0376472c2f1bf38a01eed
sittora <sittora@gmail.com>
2021-08-10 14:17:56 -0400

    Completed assignment not final

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+  return ('This one is on me!');
+} else if (feet > 2000 && feet <= 2500)  {
+  return ('I will gladly take your thirty bucks.');
+} else if (feet > 2500) {
+  return ('No can do.');
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city = 'NYC') {
+    return ('Ok, sounds good.');
+  } else if (city !== 'NYC' && city == 'Pittsburgh') {
+    return ('No go.');
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+switch(tip) {
+  case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous':
+  return 'Thank you.';
+  default:
+  return 'Bye.'
+}
 }
